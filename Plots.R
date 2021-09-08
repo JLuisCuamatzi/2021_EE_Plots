62902e0b15a30d5ce82f7972c395ee47811557b5
JLuisCuamatzi <jcuamatzi@liigh.unam.mx>
2021-09-08 12:41:17 -0500

    Code for growth plot, readygit add Plots.R

--- Plots.R
+++ Plots.R
@@ -17,9 +17,8 @@ library(lemon, warn.conflicts = F)
 library(grid, warn.conflicts = F)
 
 # reading the dataframe
-
 USMA_EE_2021 <- read_xlsx("2021_EE_Growth_data_DF.xlsx",
-                          range = "EE_Dataframe!A1:M376")
+                          range = "EE_Dataframe!A1:M376") # works
 
 # Plot: growth profile with the bottlenecks
 data <- USMA_EE_2021[,c(1:5,8,9)]
@@ -35,15 +34,72 @@ data <- data %>%
     startsWith(Cuantification, "CFU") ~ "3",
     startsWith(Cuantification, "cells") ~ "45")) # works
 
-df.temp <- data.frame(Condition = c(rep("Control",6),rep("H2O2",6)),
+df.temp <- data.frame(Condition = c(rep("Control",3),rep("H2O2",3)),
                       TreatmentNum = c("Treatment 01","Treatment 01"),
-                      H2O2 = c(rep("w/o ROS 0 mM",6), rep("5 mM",6)),
+                      H2O2 = c(rep("w/o ROS 0 mM",3), rep("5 mM",3)),
                       Selected = c("Yes","Yes"),
-                      Rep = c(rep("D",2), rep("E",2),rep("F",2),
-                              rep("A",2), rep("B",2),rep("C",2)),
-                      Cuantification = c("CFU_mL","cells_mL"),
-                      Cellular_concentration = c(0,1000000),
-                      Time = c(rep("0",12))) # works
-data <- bind_rows(data, df.temp) #works
+                      Rep = c(rep("D",1), rep("E",1),rep("F",1),
+                              rep("A",1), rep("B",1),rep("C",1)),
+                      Cuantification = c("cells_mL"),
+                      Cellular_concentration = c(1000000),
+                      Time = c(rep("1",6))) # works
+
+data <- bind_rows(df.temp,data)
+
+df.2.temp <- data.frame(Condition = c(rep("Control",21),rep("H2O2",21)),
+                        #TreatmentNum = c(paste(rep("Treatment 0",42), seq(0,20,1), sep ="")),
+                        Time = c(rep(seq(1,961,48),2)),
+                        Cellular_concentration = c(1000000))
+
+data <- data %>% 
+  arrange(TreatmentNum) %>% 
+  group_by(Rep) %>% 
+  mutate(cumulative_time = cumsum(Time)) # works??
+
+# data.a <- data[grep("A",data$Rep),] # only to see one lineage
+
+scientific <- function(x){
+  ifelse(x==0, "0", 
+         parse(text=gsub("[+]", "",gsub("e", " %*% 10^", scientific_format()(x)))))
+} # works
+
+ggplot(data, aes(x = cumulative_time, y = Cellular_concentration)) +
+  geom_line() + 
+  geom_hline(yintercept = 1e6,linetype='dotted', col = 'red')+
+  geom_hline(yintercept = 1e5,linetype='dotted', col = 'blue') +
+  geom_hline(yintercept = 1e7,linetype='dotted', col = 'orange') +
+  scale_y_log10(labels = scientific) +
+  geom_point(data = data, aes(x = cumulative_time, y = Cellular_concentration,
+                              color = Cuantification))+
+  facet_grid(Rep~.) +
+  theme_classic()+
+  geom_point(data = df.2.temp, aes(x = Time, y = Cellular_concentration))+
+  labs(y = "cells/mL", x = "Time (h)") + 
+  theme(legend.position = "none",
+        strip.text = element_text(face = "bold", size=15),
+        strip.background = element_rect(colour="black",
+                                        fill="ivory"),
+        axis.title.x = element_text(face = "bold",
+                                    size = 18),
+        axis.title.y = element_text(face = "bold",
+                                    size = 18),
+        axis.text.x = element_text(face = "bold",
+                                   size = 14),
+        axis.text.y = element_text(face = "bold",,
+                                   size = 14)) # works
 
 
+
+# data a
+ggplot(data.a, aes(x = cumulative_time, y = Cellular_concentration)) +
+  geom_line() + 
+  #geom_hline(yintercept = 1e6,linetype='dotted', col = 'red')+
+  geom_hline(yintercept = 1e4,linetype='dotted', col = 'blue')+
+  geom_hline(yintercept = 1e7,linetype='dotted', col = 'orange')+
+  #scale_y_log10() +
+  scale_y_log10(labels = scientific)+
+  #scale_y_continuous(labels = scientific)+
+  geom_point(data = data.a, aes(x = cumulative_time, y = Cellular_concentration,
+                              color = Cuantification))+
+  geom_point(data = df.2.temp, aes(x = Time, y = Cellular_concentration))+
+  theme_classic()
